accbb49ff3afb7e5ea5887c58f68b25cfdf3fb37
KukaAbob <146316346+KukaAbob@users.noreply.github.com>
2024-05-29 18:13:18 +0500

    Add files via upload

--- index.py
+++ index.py
@@ -16,6 +16,7 @@ MONGO_CONNECT_URL = os.getenv("MONGO_CONNECT_URL")
 client = MongoClient(MONGO_CONNECT_URL)
 db = client["weatherDB"]
 collection = db["forecasts"]
+
 @app.route('/')
 def index():
     return render_template("mainpage.html")
@@ -30,29 +31,23 @@ def Smart():
 
 @app.route('/monitoring')
 def monitoring():
-    # Получаем все прогнозы из базы данных
     all_forecasts = list(collection.find())
-    
-    # Получаем сегодняшнюю дату
+
     today = datetime.now().date()
 
-    # Фильтруем прогнозы, оставляя только сегодняшние
     today_forecasts = []
     for forecast in all_forecasts:
         forecast_date = datetime.strptime(forecast["DateTime"], "%Y-%m-%d").date()
         if forecast_date == today:
             existing_forecast = collection.find_one({"DateTime": forecast["DateTime"]})
             if existing_forecast:
-                # Обновляем существующий прогноз
                 collection.update_one({"_id": existing_forecast["_id"]}, {"$set": forecast})
             else:
-                # Добавляем новый прогноз
                 collection.insert_one(forecast)
             today_forecasts.append(forecast)
 
-    # Передаем данные в шаблон и рендерим страницу
     return render_template("IoT_Monitoring.html", today_forecasts=today_forecasts, all_forecasts=all_forecasts)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Используем переменную окружения PORT или порт по умолчанию 5000
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
